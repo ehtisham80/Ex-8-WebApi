8a52577c9e935df5c070ffc4ac35d68f50ee6708
Ehtisham Zaheer <ehtisham_zaheer@hotmail.com>
2020-09-08 10:58:56 +0200

    Out put is different

--- Ex-8-WebApi/Blazor/Models/CustomerCreateModel.cs
+++ Ex-8-WebApi/Blazor/Models/CustomerCreateModel.cs
@@ -9,14 +9,16 @@ namespace Blazor.Models
 {
     public class CustomerCreateModel
     {
-        [Required]
-        public int Id { get; set; }
+            
         [Required]
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
         [Required]
+        public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+
     }
 }
-}
+
